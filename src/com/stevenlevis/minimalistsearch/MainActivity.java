0b207961ad3d2996c396d60386640377667bbc07
levis501 <levis501@yahoo.com>
2013-03-13 16:16:09 -0700

    Fixed landscape mode keyboard pop-up and done.  This is the published 1.1 version .  Also added and modified drawables

--- src/com/stevenlevis/minimalistsearch/MainActivity.java
+++ src/com/stevenlevis/minimalistsearch/MainActivity.java
@@ -1,16 +1,20 @@
 package com.stevenlevis.minimalistsearch;
 
-import com.stevenlevis.minimalistsearch.R;
-
+import android.app.Activity;
+import android.app.SearchManager;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.WindowManager.LayoutParams;
+import android.view.inputmethod.EditorInfo;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
-import android.app.Activity;
-import android.app.SearchManager;
-import android.content.Intent;
+import android.widget.TextView;
+import android.widget.TextView.OnEditorActionListener;
 
 public class MainActivity extends Activity {
 
@@ -18,24 +22,36 @@ public class MainActivity extends Activity {
 	private Button mClearSearchBoxButton;
 	private Button mSearchButton;
 
+	private void launchSearch() {
+		String action = Intent.ACTION_WEB_SEARCH;
+		String query = mSearchBox.getText().toString();
+		Intent searchIntent = new Intent(action);
+		searchIntent.putExtra(SearchManager.QUERY, query);
+		startActivity(searchIntent);
+	}
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		OnClickListener go = new OnClickListener() {
+		mSearchBox = (EditText) findViewById(R.id.searchBox);
+		mSearchBox.setOnEditorActionListener(new OnEditorActionListener() {
+			@Override
+			public boolean onEditorAction(TextView v, int actionId,
+					KeyEvent event) {
+		        if (actionId == EditorInfo.IME_ACTION_DONE) {
+					launchSearch();
+					return true;
+		        }
+				return false;
+			}});
+		mSearchButton = (Button) findViewById(R.id.searchButton);
+		mSearchButton.setOnClickListener(new OnClickListener() {
 			@Override
 			public void onClick(View v) {
-				String action = Intent.ACTION_WEB_SEARCH;
-				String query = mSearchBox.getText().toString();
-				Intent searchIntent = new Intent(action);
-				searchIntent.putExtra(SearchManager.QUERY, query);
-				startActivity(searchIntent);
+				launchSearch();
 			}
-		};
-		mSearchBox = (EditText) findViewById(R.id.searchBox);
-		mSearchBox.setOnClickListener(go);
-		mSearchButton = (Button) findViewById(R.id.searchButton);
-		mSearchButton.setOnClickListener(go);
+		});
 		
 		mClearSearchBoxButton = (Button) findViewById(R.id.clearSearchBoxButton);
 		mClearSearchBoxButton.setOnClickListener( new OnClickListener() {
@@ -51,6 +67,11 @@ public class MainActivity extends Activity {
 	@Override
 	protected void onResume() {
 		this.getWindow().setSoftInputMode(LayoutParams.SOFT_INPUT_STATE_VISIBLE);
+
+		
+		InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);;
+		imm.toggleSoftInput(InputMethodManager.SHOW_FORCED,InputMethodManager.HIDE_IMPLICIT_ONLY);
+		imm.showSoftInput(mSearchBox,InputMethodManager.SHOW_FORCED);
 		
 		super.onResume();
 	}
