778b3d6d0551c82c85dbcc1d82aead2e224d7cad
levis501 <levis501@yahoo.com>
2013-04-05 08:58:23 -0700

    Fixed icons for lower-res drawables.  Removed unused assets

--- src/com/stevenlevis/minimalistsearch/MainActivity.java
+++ src/com/stevenlevis/minimalistsearch/MainActivity.java
@@ -5,6 +5,8 @@ import android.app.SearchManager;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.ClipboardManager;
+import android.util.Log;
 import android.view.KeyEvent;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -13,14 +15,18 @@ import android.view.inputmethod.EditorInfo;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.TextView.OnEditorActionListener;
 
+@SuppressWarnings("deprecation")
 public class MainActivity extends Activity {
 
 	private EditText mSearchBox;
 	private Button mClearSearchBoxButton;
-	private Button mSearchButton;
+	private ImageButton mSearchButton;
+	private ImageButton mPasteAndSearchButton;
+	private ImageButton mPasteButton;
 
 	private void launchSearch() {
 		String action = Intent.ACTION_WEB_SEARCH;
@@ -28,6 +34,22 @@ public class MainActivity extends Activity {
 		Intent searchIntent = new Intent(action);
 		searchIntent.putExtra(SearchManager.QUERY, query);
 		startActivity(searchIntent);
+	}
+	
+	private boolean insertClipboardTextAtSearchCursor() {
+		ClipboardManager clipboard = (ClipboardManager) getSystemService(CLIPBOARD_SERVICE);
+		if (clipboard.hasText()) {
+			String clipboardText = " " + clipboard.getText().toString() + " ";
+			
+			int start = mSearchBox.getSelectionStart();
+			int end = mSearchBox.getSelectionEnd();
+			mSearchBox.getText().replace(Math.min(start, end), Math.max(start,end), clipboardText, 0, clipboardText.length());
+			return true;
+		} else {
+			Log.d("MinimalistSearch","clipboard reports no text");
+			return false;
+		}
+		
 	}
 	
 	@Override
@@ -45,11 +67,29 @@ public class MainActivity extends Activity {
 		        }
 				return false;
 			}});
-		mSearchButton = (Button) findViewById(R.id.searchButton);
+		mSearchButton = (ImageButton) findViewById(R.id.searchButton);
 		mSearchButton.setOnClickListener(new OnClickListener() {
 			@Override
 			public void onClick(View v) {
 				launchSearch();
+			}
+		});
+		
+		mPasteAndSearchButton = (ImageButton) findViewById(R.id.pasteAndSearchButton);
+		mPasteAndSearchButton.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				if (insertClipboardTextAtSearchCursor()) {
+					launchSearch();
+				}
+			}
+		});
+		
+		mPasteButton = (ImageButton) findViewById(R.id.pasteButton);
+		mPasteButton.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				insertClipboardTextAtSearchCursor();
 			}
 		});
 		
