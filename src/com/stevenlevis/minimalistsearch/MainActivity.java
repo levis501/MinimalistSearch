5ce83e4b7236bd403d11ae22f16865870bb8bac4
Steven Levis <levis501@yahoo.com>
2015-03-06 16:39:38 -0800

    No longer clear text in onResume

--- src/com/stevenlevis/minimalistsearch/MainActivity.java
+++ src/com/stevenlevis/minimalistsearch/MainActivity.java
@@ -151,7 +151,7 @@ public class MainActivity extends Activity {
 
 	@Override
 	protected void onResume() {
-		mSearchBox.setText("");
+		//mSearchBox.setText("");
 		this.getWindow().setSoftInputMode(LayoutParams.SOFT_INPUT_STATE_VISIBLE);
 
 		
